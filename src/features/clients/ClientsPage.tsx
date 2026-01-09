3ce30e12819151817df845f2f59a34dfd807f61b
Chidi Mathaias <chidimathayas@gmail.com>
2026-01-09 17:30:38 +0100

    feat: add CreateClientForm component for client creation functionality

--- src/features/clients/ClientsPage.tsx
+++ src/features/clients/ClientsPage.tsx
@@ -11,9 +11,15 @@ import Loader from "@/ui/Loader";
 // Logic & Helpers
 import { getClients } from "../../services/apiClient";
 import { formatCurrency } from "@/utils/helper";
+import { AnimatePresence } from "framer-motion";
+import Modal from "@/ui/Modal";
+import CreateInvoiceForm from "../invoice/CreateInvoiceForm";
 
 const ClientsPage: React.FC = () => {
   const [searchTerm, setSearchTerm] = useState("");
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const onClose = () => setIsModalOpen(false);
 
   // Define Client type used by the query and components
   type Client = {
@@ -61,113 +67,123 @@ const ClientsPage: React.FC = () => {
   }
 
   return (
-    <div className="w-full my-10">
-      {/* ------Client overview------ */}
-      <div className="grid grid-cols-4 justify-center items-center mb-6 gap-7">
-        <div className="border border-gray-300 p-8 bg-white rounded-2xl flex flex-col justify-between items-start w-full h-[17rem]">
-          <h2 className="text-4xl font-normal">Okoya Fridaus</h2>
+    <>
+      <div className="w-full my-10">
+        {/* ------Client overview------ */}
+        <div className="grid grid-cols-4 justify-center items-center mb-6 gap-7">
+          <div className="border border-gray-300 p-8 bg-white rounded-2xl flex flex-col justify-between items-start w-full h-[17rem]">
+            <h2 className="text-4xl font-normal">Okoya Fridaus</h2>
 
-          <div className="flex flex-col gap-1">
-            <p className="font-semibold">Most Active Client</p>
-            <p className="font-normal text-[1.3rem] text-gray-400">
-              1 invoice past 30 days
-            </p>
-          </div>
-        </div>
-        <div className="border border-gray-300 p-8 bg-white rounded-2xl flex flex-col justify-between items-start w-full h-[17rem]">
-          <h2 className="text-4xl font-normal">0</h2>
-
-          <div className="flex flex-col gap-1">
-            <p className="font-semibold">Inactive Client</p>
-            <p className="font-normal  text-[1.3rem] text-gray-400">
-              No invoices or time tracked past 30 days
-            </p>
-          </div>
-        </div>
-        <div className="border border-gray-300 p-8 bg-white rounded-2xl flex flex-col justify-between items-start w-full h-[17rem]">
-          <h2 className="text-4xl font-normal">Okoya Fridaus</h2>
-
-          <div className="flex flex-col gap-1">
-            <p className="font-semibold">Top Revenue Client</p>
-            <p className="font-normal  text-[1.3rem] text-gray-400">
-              NGN 60,000.00 from 1 invoice past 30 days
-            </p>
-          </div>
-        </div>
-        <div className="border border-gray-300 p-8 bg-white rounded-2xl flex flex-col justify-between items-start w-full h-[17rem]">
-          <h2 className="text-4xl font-normal">0</h2>
-
-          <div className="flex flex-col gap-1">
-            <p className="font-semibold">New Customers</p>
-            <p className="font-normal truncate text-[1.3rem] text-gray-400">
-              Added past 30 days
-            </p>
-          </div>
-        </div>
-      </div>
-
-      {/* --- TOP BAR: SEARCH & ADD BUTTON --- */}
-      <div className="flex items-center justify-between w-full mb-6">
-        <div className="flex items-center justify-center flex-1 max-w-lg gap-4 px-4 bg-white border border-gray-300 rounded-full dark:bg-[#1e2139] dark:border-transparent transition-colors">
-          <Search className="text-gray-400" size={20} />
-          <input
-            aria-label="Search clients"
-            placeholder="Search clients by name or email..."
-            className="w-full px-4 py-3 bg-transparent outline-none dark:text-white"
-            value={searchTerm}
-            onChange={(e) => setSearchTerm(e.target.value)}
-          />
-        </div>
-
-        {/* Hook this up to your 'AddClientModal' later */}
-        <Button onClick={() => console.log("Open Add Modal")} className="">
-          Add New Client
-        </Button>
-      </div>
-
-      {/* --- TABLE CONTAINER --- */}
-      <div className="flex flex-col w-full border border-gray-200 dark:border-gray-700 rounded-2xl bg-white dark:bg-[#1e2139]  shadow-sm">
-        <ClientTable />
-
-        {/* Table Rows (The Data) */}
-        <div className="w-full">
-          {!filteredClients || filteredClients.length === 0 ? (
-            <div className="flex flex-col items-center justify-center py-20 text-gray-500">
-              <p className="text-lg">No clients found.</p>
+            <div className="flex flex-col gap-1">
+              <p className="font-semibold">Most Active Client</p>
+              <p className="font-normal text-[1.3rem] text-gray-400">
+                1 invoice past 30 days
+              </p>
             </div>
-          ) : (
-            filteredClients.map(
-              (client: NonNullable<typeof clients>[number]) => (
-                <ClientRow
-                  key={client.id}
-                  id={client.id}
-                  name={client.name}
-                  email={client.email || "--"}
-                  state={client.state}
-                  phone_number={client.phone_number}
-                  // Formatted Balance
-                  balance={formatCurrency(client.outstanding_balance)}
-                  // Formatted Date
-                  lastActive={
-                    client.last_activity
-                      ? new Date(client.last_activity).toLocaleDateString()
-                      : "N/A"
-                  }
-                  // Action Handlers
-                  onView={() => console.log("Navigating to view:", client.id)}
-                  onEdit={() =>
-                    console.log("Opening edit modal for:", client.id)
-                  }
-                  onDelete={() =>
-                    console.log("Opening delete modal for:", client.id)
-                  }
-                />
+          </div>
+          <div className="border border-gray-300 p-8 bg-white rounded-2xl flex flex-col justify-between items-start w-full h-[17rem]">
+            <h2 className="text-4xl font-normal">0</h2>
+
+            <div className="flex flex-col gap-1">
+              <p className="font-semibold">Inactive Client</p>
+              <p className="font-normal  text-[1.3rem] text-gray-400">
+                No invoices or time tracked past 30 days
+              </p>
+            </div>
+          </div>
+          <div className="border border-gray-300 p-8 bg-white rounded-2xl flex flex-col justify-between items-start w-full h-[17rem]">
+            <h2 className="text-4xl font-normal">Okoya Fridaus</h2>
+
+            <div className="flex flex-col gap-1">
+              <p className="font-semibold">Top Revenue Client</p>
+              <p className="font-normal  text-[1.3rem] text-gray-400">
+                NGN 60,000.00 from 1 invoice past 30 days
+              </p>
+            </div>
+          </div>
+          <div className="border border-gray-300 p-8 bg-white rounded-2xl flex flex-col justify-between items-start w-full h-[17rem]">
+            <h2 className="text-4xl font-normal">0</h2>
+
+            <div className="flex flex-col gap-1">
+              <p className="font-semibold">New Customers</p>
+              <p className="font-normal truncate text-[1.3rem] text-gray-400">
+                Added past 30 days
+              </p>
+            </div>
+          </div>
+        </div>
+
+        {/* --- TOP BAR: SEARCH & ADD BUTTON --- */}
+        <div className="flex items-center justify-between w-full mb-6">
+          <div className="flex items-center justify-center flex-1 max-w-lg gap-4 px-4 bg-white border border-gray-300 rounded-full dark:bg-[#1e2139] dark:border-transparent transition-colors">
+            <Search className="text-gray-400" size={20} />
+            <input
+              aria-label="Search clients"
+              placeholder="Search clients by name or email..."
+              className="w-full px-4 py-3 bg-transparent outline-none dark:text-white"
+              value={searchTerm}
+              onChange={(e) => setSearchTerm(e.target.value)}
+            />
+          </div>
+
+          {/* Hook this up to your 'AddClientModal' later */}
+          <Button onClick={() => setIsModalOpen(true)} className="">
+            Add New Client
+          </Button>
+        </div>
+
+        {/* --- TABLE CONTAINER --- */}
+        <div className="flex flex-col w-full border border-gray-200 dark:border-gray-700 rounded-2xl bg-white dark:bg-[#1e2139]  shadow-sm">
+          <ClientTable />
+
+          {/* Table Rows (The Data) */}
+          <div className="w-full">
+            {!filteredClients || filteredClients.length === 0 ? (
+              <div className="flex flex-col items-center justify-center py-20 text-gray-500">
+                <p className="text-lg">No clients found.</p>
+              </div>
+            ) : (
+              filteredClients.map(
+                (client: NonNullable<typeof clients>[number]) => (
+                  <ClientRow
+                    key={client.id}
+                    id={client.id}
+                    name={client.name}
+                    email={client.email || "--"}
+                    state={client.state}
+                    phone_number={client.phone_number}
+                    // Formatted Balance
+                    balance={formatCurrency(client.outstanding_balance)}
+                    // Formatted Date
+                    lastActive={
+                      client.last_activity
+                        ? new Date(client.last_activity).toLocaleDateString()
+                        : "N/A"
+                    }
+                    // Action Handlers
+                    onView={() => console.log("Navigating to view:", client.id)}
+                    onEdit={() =>
+                      console.log("Opening edit modal for:", client.id)
+                    }
+                    onDelete={() =>
+                      console.log("Opening delete modal for:", client.id)
+                    }
+                  />
+                )
               )
-            )
-          )}
+            )}
+          </div>
         </div>
       </div>
-    </div>
+
+      <AnimatePresence>
+        {isModalOpen && (
+          <Modal onClose={onClose} isModalOpen={isModalOpen}>
+            <CreateInvoiceForm invoiceToEdit={null} onCloseModal={onClose} />
+          </Modal>
+        )}
+      </AnimatePresence>
+    </>
   );
 };
 
---
+++ src/features/clients/CreateClientForm.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CreateClientForm = () => {
+  return <div>CreateClientForm</div>;
+};
+
+export default CreateClientForm;
